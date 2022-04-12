931fb2408063276617c3279bbfe8de34c455f3e8
komeil Parseh <ahmdparsh129@gmail.com>
2022-04-12 22:54:11 +0430

    add bash script to help in add-repo command

---
+++ repoadd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#set -x
+
+if [ "$1" == "" ];then
+  echo "repoadd.sh <repo_name>"
+  exit 1
+fi
+
+cd x86_64
+repo-add $1.db.tar.gz *pkg.tar.zst
+
+mv $1.files.tar.gz ppr.files
+mv $1.db.tar.gz ppr.db
